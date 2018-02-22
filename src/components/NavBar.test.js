c0f869a3c9d5968f1ed17f32852e7f1a618cdf07
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-02-22 17:53:26 -0500

    fix NavBar test

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,20 +1,17 @@
 import React, {PropTypes} from 'react';
 import {shallow, mount, render} from 'enzyme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import NavBar from './NavBar';
+import { version } from 'variadic.js/package.json';
 
-const muiTheme = getMuiTheme();
+
 
 describe('<NavBar />', () => {
   it('should render', () => {
     const wrapper = shallow(<NavBar />);
   });
 
-  xit('should render within muiTheme', () => {
-    //  TODO figure out how to render with muiTheme
-    const wrapper = mount(<NavBar />, {
-      context: {muiTheme},
-      childContextTypes: {muiTheme: React.PropTypes.object}
-    });
+  it('should contain version number', () => {
+    const wrapper = mount(<NavBar />);
+    expect(wrapper.text()).toContain(version);
   });
 });
