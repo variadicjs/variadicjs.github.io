23214cfe78e35ca6eec431642dc935fa631609ab
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-02-22 00:58:10 -0500

    remove variadic from App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,14 @@
 import React, {Component} from 'react';
-import variadic from 'variadic.js';
 import CardList from "./CardList";
 import NavBar from "../components/NavBar";
 
 //React.js updates - You can use this when you don't have props
 class App extends Component {
-  state={}
-
-  handleDropdownChange(value){
-    this.setState({dropdownSelection: value})
-  }
-
   render(){
-    const {dropdownSelection} = this.state;
     return(
       <div>
         <NavBar />
-        <CardList
-          variadic={variadic}
-          dropdownSelection={dropdownSelection}
-        />
+        <CardList />
       </div>
     )
   }
