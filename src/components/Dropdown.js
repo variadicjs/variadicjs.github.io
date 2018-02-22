94f0a8eb948a8a111bd0f620dae79fafe7fd0486
Andrea R <amrdev08@gmail.com>
2018-02-21 23:18:29 -0500

    fixed data output, does not push down carousel

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,17 +3,28 @@ import {Autocomplete} from 'react-materialize';
 
 const Dropdown = (props) => {
   const s = {
-    width: "40%"
+    width: "30%",
+    minWidth: "300px",
+    zIndex: "99",
+    position: "fixed",
+    marginTop: "20px",
+    marginLeft: "20px"
   }
 
-  const {handleDropdownChange, data} = props;
+  const {data} = props;
+
+  //Avoiding mutation
+  let cardData = Object.assign({}, data);
+  Object.keys(cardData).forEach(key => {
+    return cardData[key] = null 
+  });
 
   return(
     <Autocomplete
       style={s}
       title='Function'
-      data={data}
-      onAutocomplete={handleDropdownChange}
+      data={cardData}
+      onAutocomplete={props.handleDropdownChange}
     />
   )
 };
