df050a6d6f7d3e8d3d2c2b125f448f76e39fd34e
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-02-24 00:41:48 -0500

    add style to Author component

--- src/components/Author.js
+++ src/components/Author.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Author = (props) => {
   const src = `https://github.com/${props.author}.png`;
   const href = `https://github.com/${props.author}`;
+  const style = {
+    height: "30px",
+    width: "30px",
+    display: "inline-block",
+    marginTop: "18px"
+  }
 
   return (
-    <a href={href}>
+    <a style={style} href={href}>
       <img style={{borderRadius: "50%"}} src={src} alt={props.author} className="img-responsive"/>
     </a>
   );
