187f77b7cf02803dc6a4147be96ea09c7fc1c8f8
Andrea R <amrdev08@gmail.com>
2018-02-25 00:17:12 -0500

    cleanup

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Avatar = (props) => (
-    <img style={{borderRadius: "50%"}} src={props.src} alt={props.alt} className="img-responsive"/>
+    <img 
+    	style={{borderRadius: "50%"}} 
+    	src={props.src} 
+    	alt={props.alt} c
+    	lassName="img-responsive"
+    />
 );
 
 export default Avatar;
