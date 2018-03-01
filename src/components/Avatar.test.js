8f7fc01758ee3511a87a223e9980d3f24ed4f89c
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-02-28 21:39:10 -0500

    fix Avatar tests

--- src/components/Avatar.test.js
+++ src/components/Avatar.test.js
@@ -5,24 +5,17 @@ import Avatar from './Avatar';
 //  GitHub username
 const author = 'mradenovic';
 const src = `https://github.com/${author}.png`;
-const href = `https://github.com/${author}`;
 
-describe('<Author />', () => {
+describe('<Avatar />', () => {
   it('renders without crashing', () => {
-    const wrapper = shallow(<Avatar href={href} src={src} alt={author}/>);
+    const wrapper = shallow(<Avatar src={src} alt={author}/>);
   });
 
   it('contains GitHub image of the author', () => {
-    const wrapper = shallow(<Avatar href={href} src={src} alt={author}/>);
+    const wrapper = shallow(<Avatar src={src} alt={author}/>);
     const img = wrapper.find('img');
     expect(img).toBeDefined();
     expect(img.prop('src')).toEqual(src);
-  });
-
-  it('contains GitHub link of the author', () => {
-    const wrapper = shallow(<Avatar href={href} src={src} alt={author}/>);
-    const link = wrapper.find('a');
-    expect(link).toBeDefined();
-    expect(link.prop('href')).toEqual(href);
+    expect(img.prop('alt')).toEqual(author);
   });
 });
