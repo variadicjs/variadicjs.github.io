0273a70853f02b7f2ede088962d7375f19470f3e
Andrea R <amrdev08@gmail.com>
2018-04-05 20:42:13 -0400

    added contributor dropdown and avatars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import {Navbar, NavItem, Icon} from 'react-materialize';
+import {Navbar, NavItem, Icon, Dropdown} from 'react-materialize';
 import Avatar from "./Avatar";
+import "./Avatar.css";
 import "./NavBar.css";
 import { version } from 'variadic.js/package.json';
 
-const NavBar = (props) => (
+const NavBar = () => (
   <Navbar
     className="navbar-style"
-  	right
+    right
     brand={
-    	<div className="brand-div">
-    		<Avatar src="images/logo.svg" style={{height: "50px", width: "50px"}} alt="logo" /> 
-    		<span className="brand-title"> VariadicJS ({version})</span>
-    	</div>
+      <div className="brand-div">
+        <Avatar 
+          src="images/logo.svg" 
+          style={{height: "50px", width: "50px"}} 
+          alt="logo" 
+        /> 
+        <span className="brand-title"> VariadicJS ({version})</span>
+      </div>
     }
-    >
-    <NavItem>
-        <Icon medium>more_vert</Icon>
-    </NavItem>
+  >
+      
+    <Dropdown trigger={
+      <NavItem>
+        <Icon medium>people</Icon>
+      </NavItem>
+    }>
+      <NavItem>
+        <Avatar 
+          className="large-avatar round-avatar" 
+          href="https://github.com/amr08" 
+          src="https://github.com/amr08.png" 
+        />
+      </NavItem>
+      <NavItem>
+        <Avatar 
+          className="large-avatar round-avatar" 
+          href="https://github.com/jhowardjr" 
+          src="https://github.com/jhowardjr.png" 
+        />
+      </NavItem>
+      <NavItem>
+        <Avatar 
+          className="large-avatar round-avatar" 
+          href="https://github.com/mradenovic" 
+          src="https://github.com/mradenovic.png" 
+        />
+      </NavItem>
+    </Dropdown>  
   </Navbar>
 );
 
